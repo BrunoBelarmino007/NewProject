a9c56903dac95831a5b85906a352f900b12f1346
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-10-31 16:22:24 -0300

    feat: criar model e migration de Post com relacionamentos

---
+++ backend/models/post.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    static associate(models) {
+      this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+      this.hasMany(models.PostLike, { foreignKey: 'post_id', as: 'likes' });
+    }
+  }
+
+  Post.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      title: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      text: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.STRING(500),
+        allowNull: false,
+      },
+      available_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Post',
+      tableName: 'posts',
+    }
+  );
+
+  return Post;
+};
